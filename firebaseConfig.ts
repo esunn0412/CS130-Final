61e5d15e820f508ccaa94c8ba03cdf99c44af3d4
TAEEUN KIM <taeeunk1208@gmail.com>
2026-04-24 19:56:41 -0400

    setup firebase and packages

---
+++ firebaseConfig.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCuxWO8F4FXYebD8V4so2yxmzU4KBNGwz0",
+  authDomain: "emory-hacks-f3f9a.firebaseapp.com",
+  databaseURL: "https://emory-hacks-f3f9a-default-rtdb.firebaseio.com",
+  projectId: "emory-hacks-f3f9a",
+  storageBucket: "emory-hacks-f3f9a.firebasestorage.app",
+  messagingSenderId: "754783498331",
+  appId: "1:754783498331:web:9bb40ba0dd355156a5cf7e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app)
